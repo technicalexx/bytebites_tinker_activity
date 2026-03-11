8e81ce28b3ae975712d40a12e810f099aa9a2dc5
techsasha <alex.rozin@codepath.org>
2026-03-11 08:38:40 -0400

    Add empty models.py with class list

---
+++ models.py
@@ -0,0 +1,23 @@
+## Initial Classes:
+
+# ByteBites models
+# Customer stores customer information and purchase history.
+# FoodItem stores item details like name, price, category, and popularity.
+# Menu stores a collection of FoodItem objects and supports filtering/sorting.
+# Transaction stores selected items and calculates total cost.
+
+
+class Customer:
+    pass
+
+
+class FoodItem:
+    pass
+
+
+class Menu:
+    pass
+
+
+class Transaction:
+    pass
